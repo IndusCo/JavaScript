6fcb5a24dcdd96cca4dabb805c9cf97b23f7ecf8
Ahsan Tariq <165206218+IndusCo@users.noreply.github.com>
2024-04-07 19:39:43 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+// Ahsan Tariq
+// my Twenty One game (Simplified Black Jack)
+
 function myFunction(){
     document.getElementById('twentyone').innerHTML = ('Your hand equals' + " " + (sum + sum2));
 }
@@ -13,6 +16,9 @@ function myFunction3(){
 function myFunction4(){
     document.getElementById('dealer3').innerHTML = ('Your hand equals' + " " + (sum + sum2 + sum3 + sum4 + sum5));
 }
+
+
+// myFunctions present the total of your cards
 
 function generateRandomNumbers(){
 	return Math.floor(Math.random()*11)+1;
@@ -34,6 +40,8 @@ function generateRandomNumbers5(){
 	return Math.floor(Math.random()*11)+1;
 }
 
+// generateRandomNumber functions simulate the values of the "cards" dealt
+
 let randomNumberGenerated = generateRandomNumbers();
 let randomNumberGenerated2 = generateRandomNumbers2();
 let randomNumberGenerated3 = generateRandomNumbers3();
@@ -46,7 +54,11 @@ var sum3 = randomNumberGenerated3;
 var sum4 = randomNumberGenerated4;
 var sum5 = randomNumberGenerated5;
 
+// the sum variables are each card that is dealt by dealer
+
 console.log("First Two Cards equal" + " " + (sum + sum2));
+
+// this is your starting hand (two cards)
 
 if((sum + sum2) == 21){
 console.log("You Win!");
@@ -57,6 +69,8 @@ console.log("Bust!");
 else{
 console.log((sum + sum2 + sum3));
 }
+
+// the addition of the third card dealt
 
 console.log("Total Cards equal" + " " + (sum + sum2 + sum3));
 
@@ -70,6 +84,8 @@ else{
 console.log((sum + sum2 + sum3 + sum4));    
 }
 
+// the addition of the fourth card dealt
+
 console.log("Total Cards equal" + " " + (sum + sum2 + sum3 + sum4));
 
 if((sum + sum2 + sum3 + sum4) == 21){
@@ -82,6 +98,8 @@ else{
 console.log(sum + sum2 + sum3 + sum4 + sum5);
 }
 
+// the addition of the fifth card dealt
+
 console.log("Total Cards equal" + " " + (sum + sum2 + sum3 + sum4 + sum5));
 if((sum + sum2 + sum3 + sum4 + sum5) == 21){
 console.log("You Win!");
@@ -89,3 +107,5 @@ console.log("You Win!");
 else if((sum + sum2 + sum3 + sum4 + sum5) > 21){
 console.log("Bust!");
 }
+
+// evaluation of the fifth card to the hand
